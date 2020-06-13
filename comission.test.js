b17b56e45933cf26c783948ae9b4bfc07a2ab34c
sergey.gerasimov <serger777@gmail.com>
2020-06-13 22:06:17 +0300

    add jest

--- comission.test.js
+++ comission.test.js
@@ -1,28 +1,60 @@
 const { commission } = require('./comission.js');
 
 const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
-
-const test = (name, day, result)=>{
-    const dateFly = Date.now() + MILLISECONDS_IN_DAY* day ;
-    if(commission(dateFly) === result){
-        console.log( `${name} success`);
-    }else{
-        console.log( `${name} error`);
-    }
+const dateFly = (day)=>{
+    return Date.now() + day * MILLISECONDS_IN_DAY
 };
+const second = 1000;
 
-test('после вылета', -1, 100);
-test('до 24 часов', 0, 75);
 
-test('дата 1 день', 1, 50);
+describe(" после вылета и в день  вылета",()=>{
 
-test('дата 4 дня', 4, 50);
-test('дата 5 дней', 5, 20);
-test('дата 6 дней', 6, 20);
+    test('после вылета', ()=>{
+        expect(commission(dateFly(0)-second)).toBe(100);
+    });
+    test('0 + 1', ()=>{
+        expect(commission(dateFly(0)+second)).toBe(75);
+    });
+    test('0 дней', ()=>{
+        expect(commission(dateFly(0))).toBe(75);
+    });
+    test('день - секунда', ()=>{
+        expect(commission(dateFly(1)-second)).toBe(75);
+    });
+});
+describe(" за 24 часа",()=>{
+   test('за день', ()=>{
+       expect(commission(dateFly(1))).toBe(50);
+   });
+    test('день +', ()=>{
+        expect(commission(dateFly(1))).toBe(50);
+    });
+    test('5 дней - 1', ()=>{
+        expect(commission(dateFly(5)-second)).toBe(50);
+    });
+});
+describe("за 5 дней ",()=>{
+    test('за 5 дней', ()=>{
+        expect(commission(dateFly(5))).toBe(20);
+    });
+    test('5 дней + 1', ()=>{
+        expect(commission(dateFly(5)+second)).toBe(20);
+    });
+    test('10 дней -1', ()=>{
+        expect(commission(dateFly(10)-second)).toBe(20);
+    });
+});
+describe("10 дней до вылета ",()=>{
+    test('за 10 дней', ()=>{
+        expect(commission(dateFly(10))).toBe(0);
+    });
+    test('10 дней + 1', ()=>{
+        expect(commission(dateFly(10)+second)).toBe(0);
+    });
+});
 
-test('дата 9 дней', 9, 20);
-test('дата 10 дней', 10, 0);
-test('дата 11 дней', 11, 0);
-
-test('test', "test", 0);
-
+describe("не число",()=>{
+    test('не число', ()=>{
+        expect(commission(dateFly("sdsd"))).toBeFalsy();
+    });
+});
